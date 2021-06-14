36b2ee901d73943f2bdf84f03917ca6ae3597709
Atiqur <atiqrs.me@gmail.com>
2021-06-14 16:59:10 +0600

    detect range of circle

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 
-karnel = np.ones((20,20),np.uint8)
+karnel = np.ones((2,2),np.uint8)
 
-img = cv2.imread("res/off.png",1)
+img = cv2.imread("res/fon.jpg",1)
+
 imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGrey,(11,11),0)
 imgEdge = cv2.Canny(imgBlur, 20,50)
@@ -11,12 +12,12 @@ imgDialation = cv2.dilate(imgEdge,karnel,iterations=1)
 
 imagem = cv2.bitwise_not(imgDialation)
 
-cv2.imshow("img main", img)
-cv2.imshow("img Gray", imgGrey)
-cv2.imshow("img GaussianBlur", imgBlur)
+# cv2.imshow("img main", img)
+# cv2.imshow("img Gray", imgGrey)
+# cv2.imshow("img GaussianBlur", imgBlur)
 cv2.imshow("img Canny", imgEdge)
-cv2.imshow("img Canny", imgDialation)
-cv2.imshow("img Revert", imagem)
+cv2.imshow("img Dialation", imgDialation)
+# cv2.imshow("img Revert", imagem)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- test2.py
+++ test2.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 
-karnel = np.ones((20,20),np.uint8)
+karnel = np.ones((2,2),np.uint8)
+# karnel = np.ones((20,20),np.uint8)
+cir_count = 0
 
-
-img = cv2.imread("res/off.png",1)
+img = cv2.imread("res/fon.jpg",1)
 imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGrey,(11,11),0)
 imgEdge = cv2.Canny(imgBlur, 20,50)
@@ -12,8 +13,15 @@ imgDialation = cv2.dilate(imgEdge,karnel,iterations=1)
 
 imagem = cv2.bitwise_not(imgDialation)
 
-_, thrash = cv2.threshold(imagem, 240, 255, cv2.THRESH_BINARY)
-contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+# cv2.imshow("img main", img)
+# cv2.imshow("img Gray", imgGrey)
+# cv2.imshow("img GaussianBlur", imgBlur)
+# cv2.imshow("img Canny", imgEdge)
+# cv2.imshow("img Canny", imgDialation)
+# cv2.imshow("img Revert", imagem)
+
+_, thrash = cv2.threshold(imagem, 240, 255, cv2.THRESH_BINARY_INV)
+contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 cv2.imshow("img", img)
 for contour in contours:
@@ -36,9 +44,12 @@ for contour in contours:
     # elif len(approx) == 10:
     #     cv2.putText(img, "Star", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
     else:
+        cir_count += 1
+        print(cir_count)
+        str = "No ", cir_count, " Circle"
         cv2.putText(img, "Circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
 
 
-cv2.imshow("shapes", img)
+# cv2.imshow("shapes", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
